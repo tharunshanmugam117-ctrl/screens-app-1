cbf1d265ac327e0bb9a6922964a1988a4eb0f268
Tharun Shanmugam <tharunshanmugam117@gmail.com>
2026-07-04 14:22:37 +0800

    Initial Commit

---
+++ ScreensApp1/ScreensApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  ScreensApp1App.swift
+//  ScreensApp1
+//
+//  Created by T Krobot on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreensApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
