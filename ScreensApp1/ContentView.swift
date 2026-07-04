a54683a0f61b641d3fe805e844fe4c20ad969ab0
Tharun Shanmugam <tharunshanmugam117@gmail.com>
2026-07-04 14:46:36 +0800

    Added name and background color

--- ScreensApp1/ContentView.swift
+++ ScreensApp1/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+            Text("Tharun")
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+            
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
